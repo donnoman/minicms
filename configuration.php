1059f5ddeefcb7d6bdcc5efa803966a44a24b964
Donovan Bray <donnoman@donovanbray.com>
2011-03-06 16:10:07 -0800

    removed the headers to make the package easier to maintain

--- configuration.php
+++ configuration.php
@@ -1,21 +1,6 @@
 <?php
 /**************************************************
   MiniCMS Plugin for Coppermine Photo Gallery
-  *************************************************
-  MiniCMS
-  Copyright (c) 2005-2006 Donovan Bray <donnoman@donovanbray.com>
-  *************************************************
-  1.3.0  eXtended miniCMS
-  Copyright (C) 2004 Michael Trojacher <m.trojacher@webtips.at>
-  Original miniCMS Code (c) 2004 by Tarique Sani <tarique@sanisoft.com>,
-  Amit Badkas <amit@sanisoft.com>
-  *************************************************
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-  *************************************************
-  Coppermine version: 1.4.x
 ***************************************************/
 global $CONFIG;
 $name='MiniCMS';
@@ -29,14 +14,14 @@ if (file_exists("plugins/minicms/lang/{$CONFIG['lang']}.php")) {
 } else require 'plugins/minicms/lang/english.php';
 
 $install_info=<<<EOT
-    <a href="plugins/minicms/README" title="README" class="admin_menu">README</a>&nbsp;
-    <a href="plugins/minicms/CHANGELOG" title="CHANGELOG" class="admin_menu">CHANGELOG</a>&nbsp;
+    <a href="plugins/minicms/readme.txt" title="README" class="admin_menu">README</a>&nbsp;
+    <a href="plugins/minicms/changelog.txt" title="CHANGELOG" class="admin_menu">CHANGELOG</a>&nbsp;
     <a href="index.php?file=minicms/cms_config" title="{$lang_minicms['config_title']}" class="admin_menu">{$lang_minicms['config_title']}</a>
 EOT;
 
 $extra_info = <<<EOT
-    <a href="plugins/minicms/README" title="README" class="admin_menu">README</a>&nbsp;
-    <a href="plugins/minicms/CHANGELOG" title="CHANGELOG" class="admin_menu">CHANGELOG</a>
+    <a href="plugins/minicms/readme.txt" title="README" class="admin_menu">README</a>&nbsp;
+    <a href="plugins/minicms/changelog.txt" title="CHANGELOG" class="admin_menu">CHANGELOG</a>
 EOT;
 
 ?>
--- include/themes.inc.php
+++ include/themes.inc.php
@@ -1,25 +1,6 @@
 <?php
 /**************************************************
-  CPG MiniCMS Plugin for Coppermine Photo Gallery
-  *************************************************
-  CPGMiniCMS
-  Copyright (c) 2005-2006 Donovan Bray <donnoman@donovanbray.com>
-  *************************************************
-  1.3.0  eXtended miniCMS
-  Copyright (C) 2004 Michael Trojacher <m.trojacher@webtips.at>
-  Original miniCMS Code (c) 2004 by Tarique Sani <tarique@sanisoft.com>,
-  Amit Badkas <amit@sanisoft.com>
-  *************************************************
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-  *************************************************
-  Coppermine version: 1.4.x
-  $Source: /cvsroot/cpg-contrib/minicms/include/themes.inc.php,v $
-  $Revision: 1.8 $
-  $Author: donnoman $
-  $Date: 2006/11/10 21:24:24 $
+  MiniCMS Plugin for Coppermine Photo Gallery
 ***************************************************/
 
 if (!defined('IN_COPPERMINE')) { die('Not in Coppermine...');}
